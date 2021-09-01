b9552fe6c72565dc5f183876669996528688f8b0
Zachary Goodman <znote89@gmail.com>
2021-08-31 22:18:39 -0400

    commit tree d593b8b459b4123c8c8c78667b55f7f391f43c84

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,15 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
+  private
+
+  def movie_params
+    params.permit(:ittle, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
 end
